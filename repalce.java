b1b5878b45facee3d7aa7ab6eee7d2ceadec7cfb
HarrisAllwin <harrisallwin25@gmail.com>
2016-07-21 14:37:27 +0530

    Add files via upload

---
+++ repalce.java
@@ -0,0 +1,10 @@
+
+public class repalce {
+	public static void main(String[] args) {
+		String repalce="wipro tech";
+		String replaceString=repalce.replaceAll("i", "z");
+		System.out.println(replaceString);
+ 
+	}
+
+}
